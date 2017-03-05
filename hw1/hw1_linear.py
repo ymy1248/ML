0033d735bb0e0184da7e679dff392445ab4932ca
ymy1248 <r04921094@ntu.edu.tw>
2017-03-06 02:08:24 +0800

    5.79

--- hw1/hw1_linear.py
+++ hw1/hw1_linear.py
@@ -3,15 +3,16 @@
 import csv
 import math
 import time
+# import matplotlib.pyplot as plt
 
 start = time.time()
 w = []
 b = 0.0
-e = 100					# tolerance
-rate = 100
+e = 0.001				# tolerance
+rate = 0.005
 b_rate = 0.0
 w_rate = []
-order = 2
+order = 3
 
 
 def init_w (order):
@@ -50,6 +51,7 @@ def model_function(order, in_put):
 		for j in range(order):
 			power *= in_put[i]
 			out_put += w[i*order+j] * power
+	# return round(out_put)
 	return out_put
 
 def gradient_func(order, in_put_all):
@@ -64,10 +66,10 @@ def gradient_func(order, in_put_all):
 		gradient[order*9] += -2 * diff
 	return gradient
 
-def loss_function(data, model_func = first_order):
+def loss_function(order, data, model_func = model_function):
 	loss = 0.0
 	for d in data:
-		loss +=  (d[9] - model_func(d[:9]))**2
+		loss +=  (d[9] - model_func(order ,d[:9]))**2
 	return loss
 
 def decent(order, data, gradient = gradient_func, tolerance = 0.00001):
@@ -78,29 +80,34 @@ def decent(order, data, gradient = gradient_func, tolerance = 0.00001):
 	gra = gradient(order, data) 
 	# gra = first_gradient(data)
 	flag = True
-	j = 0
+	loss = []
+	k = 0
 	while flag == True:
-	# for k in range(100):
-		if j == 2000:
-			print(gra)
-			print("weight:", w)
-			print("b:", b)
-			print("-------------------------------------------------")
-			j = 0
+	# for k in range(3000):
+		if k%1000 == 0:
+			loss_num = loss_function(order, data)
+			loss.append(loss_num)
+			print("k = ", k)
+			print("loss: ",loss_num)
+			print("gradient: ", gra)
+			print("weight: ", w)
+			print("b: ", b)
+			print("----------------------------------------")
 		for i in range(len(w)):
 			w_rate[i] += gra[i]**2
 			w[i] -= rate / math.sqrt(w_rate[i]) * gra[i]
 		b_rate += gra[len(gra)-1]**2
 		b -= rate/ math.sqrt(b_rate)*gra[len(gra)-1]
 		gra = gradient(order, data)
-		j = j + 1
 		# gra = first_gradient(data)
 		# print(gra)
 		flag = False
+		k = k + 1
 		for i in gra:
 			if abs(i) >= tolerance:
 				flag = True
 				break
+	# plt.plot(loss)
 	print("weight:", w)
 	print("b:", b)
 
@@ -135,14 +142,15 @@ for i in range(9, len(test_raw_data), 18):
 		per_data.append(float(test_raw_data[i][j]))
 	test_data.append(per_data)
 
+w = [0.04585533860191334, 0.006552716549903076, -8.120804715120626e-05, 0.04446749125074442, -0.004612204643310099, 4.159805740546363e-05, 0.04773493112952491, -0.0020201707937992316, 3.42417297603347e-05, 0.04644202404579598, -0.004032051524542142, 2.1991808607624977e-05, 0.04758202243627979, -0.004458581288049339, 3.2067622903531174e-05, 0.052270127040523126, 0.011872061727434737, -7.518900040198121e-05, 0.04038685044025064, -0.012595520600392946, 7.230271378424232e-05, 0.058556741639989765, -0.006192885660041174, 5.3191494420796614e-05, 0.289914290368677, 0.02702273124607806, -0.0002072555381747574]
+b = 1.6453748248765698
+# init_w(order)
 init_w_rate(order)
-init_w(order)
 decent(order, train_data, gradient_func, e)
-
 ans = []
 
 for i in range(len(test_data)):
-	ans.append(first_order(test_data[i]))
+	ans.append(model_function(order,test_data[i]))
 	test_data[i].append(ans[i])
 
 
@@ -152,6 +160,7 @@ with open("ans.csv", "w") as f:
 	for i in range(len(ans)):
 		row = ["id_"+str(i), ans[i]]
 		writer.writerow(row)
-print(loss_function(train_data))
-end = time.time()
-print("time: ", end - start)
+# plt.show()
+print(loss_function(order, train_data))
+# end = time.time()
+# print("time: ", end - start)
--- hw1/python_test.py
+++ hw1/python_test.py
@@ -1,5 +1,8 @@
-w = 10
-def foo(w):
-	print(w)
-foo(5)
-print(w)
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4])
+plt.plot([3,4,5,6])
+plt.plot([6,7,8,8])
+plt.plot([7,8,9,10])
+plt.plot([8,9,10,11])
+plt.ylabel('some numbers')
+plt.show()
